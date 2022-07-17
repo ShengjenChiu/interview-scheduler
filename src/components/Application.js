890f59f6c0117b1265ae4f37f62f21fa9371db8b
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-07-17 09:12:09 -0600

    update states of days

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,95 +3,95 @@ import Appointment from "./Appointment/index";
 import "components/Application.scss";
 import DayList from "./DayList";
 import axios from "axios";
-//import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+
 
 const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  },
-  "last": {
-    id: "last",
-    time: "5pm",
-  }
+  // "1": {
+  //   id: 1,
+  //   time: "12pm",
+  // },
+  // "2": {
+  //   id: 2,
+  //   time: "1pm",
+  //   interview: {
+  //     student: "Lydia Miller-Jones",
+  //     interviewer:{
+  //       id: 3,
+  //       name: "Sylvia Palmer",
+  //       avatar: "https://i.imgur.com/LpaY82x.png",
+  //     }
+  //   }
+  // },
+  // "3": {
+  //   id: 3,
+  //   time: "2pm",
+  // },
+  // "4": {
+  //   id: 4,
+  //   time: "3pm",
+  //   interview: {
+  //     student: "Archie Andrews",
+  //     interviewer:{
+  //       id: 4,
+  //       name: "Cohana Roy",
+  //       avatar: "https://i.imgur.com/FK8V841.jpg",
+  //     }
+  //   }
+  // },
+  // "5": {
+  //   id: 5,
+  //   time: "4pm",
+  // },
+  // "last": {
+  //   id: "last",
+  //   time: "5pm",
+  // }
 };
 
 
-const interviewers = [
-  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-];
+// const interviewers = [
+//   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+//   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+//   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+//   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+//   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+// ];
 
 
 export default function Application() {
   // const [interviewer, setInterviewer] = useState(null);
 
-  const [day, setDay] = useState('Monday');
-  const [days, setDays] = useState([]);
-  // const [appointments, setAppointments] = useState({})
+  // const [day, setDay] = useState('Monday');
+  // const [days, setDays] = useState([]);
+  //const [appointments, setAppointments] = useState({})
 
   // const setADay = () => setDay(days.name);
   //const setCurrentInterviewer = () => setInterviewer(interviewer.id);
 
-  // const [state, setState] = useState({
-  //   day: "Monday",
-  //   days: [],
-  //   appointments: {}
-  // });
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
 
   // const dailyAppointments = [];
 
   // dailyAppointments = getAppointmentsForDay(state, state.day)
   
 
-  //create a new object for: When we click on a day in the sidebar, 
-  //we need to update the day state. 
-  //We want to do this while retaining the state for days and appointments.
-  // const state = { day: "Monday", days: [] };
-  // setState({ ...state, day: "Tuesday" });
+  //click on a day in the sidebar
+  //To update the day state and retainning the state for days and appointments
+  //so, we need to create new object to be called
+  //to update the state with new day
+  const setDay = day => setState({ ...state, day });
 
-  // const setDay = day => setState({ ...state, day });
+  //to update the state with new days
+  const setDays = (days) => {
+    // setState({ ...state, days });
+    setState(prev => ({ ...prev, days }));
+  }
 
-  // const setDays = (days) => {
-  //   ... your code here ...
-  // }
-
-  // setState(prev => ({ ...prev, days }));
 
 
   useEffect(() => {
@@ -100,9 +100,9 @@ export default function Application() {
     console.log(response.data);
     setDays(response.data)
   });
-    //  axios.get(' /api/appointments', {})
+  //   axios.get(' /api/appointments')
   // .then(response => setDays(response.data));
-  //      axios.get(' /api/interviewers', {})
+  //      axios.get(' /api/interviewers')
   // .then(response => setDays(response.data));
   }, []);
 
@@ -152,6 +152,9 @@ export default function Application() {
     );
   });
 
+  // console.log(appointments);
+
+  // console.log( Object.values(appointments));
 
   return (
     <main className="layout">
@@ -164,8 +167,8 @@ export default function Application() {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
       <DayList
-        days={days}
-        value={day}
+        days={state.days}
+        value={state.day}
         onChange={setDay}
       />
       </nav>
@@ -181,9 +184,3 @@ export default function Application() {
     </main>
   );
 }
-
-// {/* <DayList
-//     days={state.days}
-//     day={state.day}
-//     setDay={.....}
-// /> */}
